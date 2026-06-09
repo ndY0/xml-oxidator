2068a3fc486bb3ddfc9f18c07755dbdbbb23ac23
Andy Crépin <andy.crepin@soprasteria.com>
2026-06-09 18:56:17 +0200

    feat: refacto rule + builder

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,47 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 
-use crate::nodebuilder::{CutpointStrategy, NodeBuilder, NodeView, Path, Root, Strategy};
+use crate::nodebuilder::{NodeBuilder, NodeView, Path, Root, RuleBuilder};
 
 pub mod nodebuilder;
 
+struct Accumulator {
+    pub value: f32
+}
 
 fn test() {
 
-    let parent_mapper = |view: NodeView, ctx: &mut HashMap<String, String>| {
+    let parent_mapper = |view: &NodeView, ctx: &mut HashMap<String, String>| {
         // impl mapping
     };
 
+    let rule_builder = RuleBuilder::test(Arc::new(|view, ctx| {
+                view.attr("UUID").map_or(false, |&id| {id == "un id".into()})
+            }))
+            .fold(Arc::new(|acc, curr| {
+                acc || curr
+            }))
+            .init(Arc::new(|| { false }))
+            .assert(Arc::new(|res| {*res}));
+
     let test = Root::new()
-    .add_rule(rule)
+    .add_rule(
+        rule_builder.build("nous rencontrons un pb de check sur l'identifiant")
+    )
         .path(
             Path::Child("Invoice".into()),
-            Strategy::CutPoint(CutpointStrategy::Group(10)),
             Some(&parent_mapper)
         )
-        .add_rule(rule)
-            .path(Path::Child("Address".into()), Strategy::Passthrough, None)
-            .add_rule(rule)
+        .add_rule(
+            rule_builder.build("nous rencontrons un pb de check sur l'identifiant")
+        )
+            .path(Path::Child("Address".into()), None)
+            .add_rule(
+                rule_builder.build("nous rencontrons un pb de check sur l'identifiant")
+            )
             .build()
-        .add_rule(rule)
+        .add_rule(
+            rule_builder.build("nous rencontrons un pb de check sur l'identifiant")
+        )
         .build()
     .build();
 }
--- src/nodebuilder.rs
+++ src/nodebuilder.rs
@@ -1,4 +1,6 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, future::Future, marker::PhantomData, pin::Pin, sync::Arc};
+
+use futures::{Stream, StreamExt};
 
 pub trait NodeHandler<'a> {
     type ToBuilderOuput;
@@ -6,12 +8,12 @@ pub trait NodeHandler<'a> {
     fn to_builder(self) -> Self::ToBuilderOuput;
 }
 
-pub trait NodeBuilder<'a, NodeView> {
+pub trait NodeBuilder<'a> {
     type AddRuleOutput;
     type PathOutput;
     type BuildOutput;
-    fn add_rule(self, rule: Rule<NodeView>) -> Self::AddRuleOutput;
-    fn path(self, path: Path, strategy: Strategy, map_from_parent: Option<&'a ParentPropertyMapper<'a>>) -> Self::PathOutput;
+    fn add_rule(self, rule: Rule) -> Self::AddRuleOutput;
+    fn path(self, path: Path, map_from_parent: Option<&'a ParentPropertyMapper<'a>>) -> Self::PathOutput;
     fn build(self) -> Self::BuildOutput;
 }
 
@@ -23,10 +25,9 @@ struct NodeAdderState;
 
 pub struct Root<'a, State> {
     _state: std::marker::PhantomData<State>,
-    strategy: Strategy,
     path: Path,
     nodes: HashMap<Path, (Node<'a>, Option<&'a ParentPropertyMapper<'a>>)>,
-    rules: Vec<Rule<SingleNodeView>>
+    rules: Vec<Rule>
 }
 
 impl <'a> NodeHandler<'a> for Root<'a, NodeAdderState> {
@@ -38,7 +39,6 @@ impl <'a> NodeHandler<'a> for Root<'a, NodeAdderState> {
         Root {
             _state: std::marker::PhantomData,
             nodes: self.nodes,
-            strategy: self.strategy,
             path: self.path,
             rules: self.rules
         }
@@ -57,20 +57,18 @@ impl <'a> NodeBuilder<'a> for Root<'a, InitState> {
         self
     }
 
-    fn path(self, path: Path, strategy: Strategy, map_from_parent: Option<&'a ParentPropertyMapper<'a>>) -> Self::PathOutput {
+    fn path(self, path: Path, map_from_parent: Option<&'a ParentPropertyMapper<'a>>) -> Self::PathOutput {
         let build_parent: Root<NodeAdderState> = Root {
             _state: std::marker::PhantomData,
             nodes: self.nodes,
             path: self.path,
-            strategy: self.strategy,
             rules: self.rules
         };
-        Child::new(build_parent, path, strategy, map_from_parent)
+        Child::new(build_parent, path, map_from_parent)
     }
 
     fn build(self) -> Self::BuildOutput {
         Node::new(
-            self.strategy,
             self.rules,
             self.nodes
         )
@@ -81,7 +79,6 @@ impl <'a> Root<'a, InitState> {
     pub fn new() -> Self {
         Self {
             _state: std::marker::PhantomData,
-            strategy: Strategy::Passthrough,
             path: Path::Root,
             nodes: HashMap::new(),
             rules: Vec::new()
@@ -89,14 +86,13 @@ impl <'a> Root<'a, InitState> {
     }
 }
 
-pub struct Child<'a, State, Parent, NodeView> {
+pub struct Child<'a, State, Parent> {
     _state: std::marker::PhantomData<State>,
-    strategy: Strategy,
     map_from_parent: Option<&'a ParentPropertyMapper<'a>>,
     parent: Parent,
     path: Path,
     nodes: HashMap<Path, (Node<'a>, Option<&'a ParentPropertyMapper<'a>>)>,
-    rules: Vec<Rule<NodeView>>
+    rules: Vec<Rule>
 }
 
 impl <'a, P: NodeHandler<'a>> NodeHandler<'a> for Child<'a, NodeAdderState, P> {
@@ -114,7 +110,6 @@ impl <'a, P: NodeHandler<'a>> NodeHandler<'a> for Child<'a, NodeAdderState, P> {
             map_from_parent: self.map_from_parent,
             path: self.path,
             parent: self.parent,
-            strategy: self.strategy,
             rules: self.rules
         }
     }
@@ -129,7 +124,6 @@ impl <'a, P: NodeHandler<'a>> Child<'a, NodeAdderState, P> {
             parent: self.parent,
             path: self.path,
             rules: self.rules,
-            strategy: self.strategy
         }
     }
 }
@@ -146,7 +140,7 @@ impl <'a, P: NodeHandler<'a>> NodeBuilder<'a> for Child<'a, InitState, P> {
         self
     }
 
-    fn path(self, path: Path, strategy: Strategy, map_from_parent: Option<&'a ParentPropertyMapper<'a>>) -> Self::PathOutput
+    fn path(self, path: Path, map_from_parent: Option<&'a ParentPropertyMapper<'a>>) -> Self::PathOutput
         where Child<'a, NodeAdderState, P>: NodeHandler<'a>
     {
         let build_parent = Child {
@@ -155,15 +149,13 @@ impl <'a, P: NodeHandler<'a>> NodeBuilder<'a> for Child<'a, InitState, P> {
             parent: self.parent,
             nodes: self.nodes,
             path: self.path,
-            strategy: self.strategy,
             rules: self.rules
         };
-        Child::new(build_parent, path, strategy, map_from_parent)
+        Child::new(build_parent, path, map_from_parent)
     }
 
     fn build(mut self) -> Self::BuildOutput {
         let current_node = Node::new(
-            self.strategy,
             self.rules,
             self.nodes
         );
@@ -177,12 +169,10 @@ impl <'a, P: NodeHandler<'a>> Child<'a, InitState, P> {
     pub fn new(
         parent: P,
         path: Path,
-        strategy: Strategy,
         map_from_parent: Option<&'a ParentPropertyMapper<'a>>
     ) -> Child<'a, InitState, P> {
         Child {
             _state: std::marker::PhantomData,
-            strategy,
             path,
             map_from_parent,
             parent,
@@ -192,62 +182,174 @@ impl <'a, P: NodeHandler<'a>> Child<'a, InitState, P> {
     }
 }
 
-pub type ParentPropertyMapper<'a> = dyn Fn(NodeView, &'a mut HashMap<String, String>);
+pub type ParentPropertyMapper<'a> = dyn Fn(&NodeView, &'a mut HashMap<String, String>);
 
-pub struct Node<'a, NodeView> {
-    strategy: Strategy,
-    rules: Vec<Rule<NodeView>>,
+pub struct Node<'a> {
+    rules: Vec<Rule>,
     nodes: HashMap<Path, (Node<'a>, Option<&'a ParentPropertyMapper<'a>>)>,
 
 }
 
-impl <'a, NodeView> Node<'a, NodeView> {
+impl <'a> Node<'a> {
     pub fn new(
-        strategy: Strategy,
-        rules: Vec<Rule<NodeView>>,
+        rules: Vec<Rule>,
         nodes: HashMap<Path, (Node<'a>, Option<&'a ParentPropertyMapper<'a>>)>
     ) -> Self {
         Self {
-            strategy,
             rules,
             nodes
         }
     }
 }
 
-struct Rule<NodeView> {
-    test: Box<dyn Fn(NodeView, HashMap<String, String>) -> bool>,
+struct NoTest;
+struct NoFold;
+struct NoInit;
+struct NoAssert;
+
+pub struct RuleBuilder<
+    TestType,
+    FoldType,
+    InitType,
+    AssertType
+> {
+    test: TestType,
+    fold: FoldType,
+    init: InitType,
+    assert: AssertType,
+}
+impl RuleBuilder<
+    NoTest,
+    NoFold,
+    NoInit,
+    NoAssert
+> {
+    pub fn test<R>(test: Arc<dyn Fn(&NodeView, &HashMap<String, String>) -> R>) -> RuleBuilder<
+        Arc<dyn Fn(&NodeView, &HashMap<String, String>) -> R>,
+        NoFold,
+        NoInit,
+        NoAssert
+    > {
+        RuleBuilder {
+            test: test,
+            fold: NoFold,
+            init: NoInit,
+            assert: NoAssert
+        }
+    }
+}
+
+impl <R: 'static> RuleBuilder<
+    Arc<dyn Fn(&NodeView, &HashMap<String, String>) -> R>,
+    NoFold,
+    NoInit,
+    NoAssert
+> {
+    pub fn fold<Acc>(self, fold: Arc<dyn Fn(Acc, R) -> Acc>) -> RuleBuilder<
+        Arc<dyn Fn(&NodeView, &HashMap<String, String>) -> R>,
+        Arc<dyn Fn(Acc, R) -> Acc>,
+        NoInit,
+        NoAssert
+    > {
+        RuleBuilder {
+            test: self.test,
+            fold: fold,
+            init: NoInit,
+            assert: NoAssert
+        }
+    }
+}
+
+impl <R: 'static, Acc: 'static> RuleBuilder<
+    Arc<dyn Fn(&NodeView, &HashMap<String, String>) -> R>,
+    Arc<dyn Fn(Acc, R) -> Acc>,
+    NoInit,
+    NoAssert
+> {
+    pub fn init(self, init: Arc<dyn Fn() -> Acc>) -> RuleBuilder<
+        Arc<dyn Fn(&NodeView, &HashMap<String, String>) -> R>,
+        Arc<dyn Fn(Acc, R) -> Acc>,
+        Arc<dyn Fn() -> Acc>,
+        NoAssert
+    > {
+        RuleBuilder {
+            test: self.test,
+            fold: self.fold,
+            init: init,
+            assert: NoAssert
+        }
+    }
+}
+
+impl <R: 'static, Acc: 'static> RuleBuilder<
+    Arc<dyn Fn(&NodeView, &HashMap<String, String>) -> R>,
+    Arc<dyn Fn(Acc, R) -> Acc>,
+    Arc<dyn Fn() -> Acc>,
+    NoAssert
+> {
+    pub fn assert(self, assert: Arc<dyn Fn(&Acc) -> bool>) -> RuleBuilder<
+        Arc<dyn Fn(&NodeView, &HashMap<String, String>) -> R>,
+        Arc<dyn Fn(Acc, R) -> Acc>,
+        Arc<dyn Fn() -> Acc>,
+        Arc<dyn Fn(&Acc) -> bool>
+    > {
+        RuleBuilder {
+            test: self.test,
+            fold: self.fold,
+            init: self.init,
+            assert: assert
+        }
+    }
+}
+
+impl <R: 'static, Acc: 'static> RuleBuilder<
+    Arc<dyn Fn(&NodeView, &HashMap<String, String>) -> R>,
+    Arc<dyn Fn(Acc, R) -> Acc>,
+    Arc<dyn Fn() -> Acc>,
+    Arc<dyn Fn(&Acc) -> bool>
+> {
+    pub fn build(&self, assertion: &str) -> Rule {
+        let cloned_test = Arc::clone(&self.test);
+        let cloned_fold = Arc::clone(&self.fold);
+        let cloned_assert = Arc::clone(&self.assert);
+        let cloned_init = Arc::clone(&self.init);
+        Rule {
+            assertion: assertion.into(),
+            test_fold_assert: Box::new(
+                move |mut stream, ctx| {
+                        let mut init = cloned_init();
+                        let cloned_test = Arc::clone(&cloned_test);
+                        let cloned_fold = Arc::clone(&cloned_fold);
+                        let cloned_assert = Arc::clone(&cloned_assert);
+                    Box::pin(async move {
+                        while let Some(view) = stream.next().await {
+                            init = cloned_fold(init, cloned_test(&view, ctx))
+                        }
+                        cloned_assert(&init)
+                    })
+                }
+            )
+        }
+    }
+}
+
+pub struct Rule {
+    test_fold_assert: Box<dyn for<'a> Fn(Pin<Box<dyn Stream<Item = NodeView> + 'a>>, &'a HashMap<String, String>) -> Pin<Box<dyn Future<Output = bool> + 'a>>>,
     assertion: String
 }
 
-pub struct SingleNodeView {
+pub struct NodeView {
     text: Option<String>,
     attrs: HashMap<String, String>
 }
 
-impl SingleNodeView {
-    fn text(&self) -> Option<&String> {
+impl NodeView {
+    pub fn text(&self) -> Option<&String> {
         self.text.as_ref()
     }
-    fn attr(&self, key: &str) -> Option<&String> {
+    pub fn attr(&self, key: &str) -> Option<&String> {
         self.attrs.get(key)
     }
-}
-
-pub struct ListNodeView {
-    from: usize,
-    to: usize,
-    views: Vec<SingleNodeView>
-}
-
-pub enum Strategy {
-    Passthrough,
-    CutPoint(CutpointStrategy),
-}
-
-pub enum CutpointStrategy {
-    Unit,
-    Group(u32)
 }
 
 #[derive(PartialEq, Eq, Hash)]
